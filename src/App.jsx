0ea8938054d34f31dee1a76252787e926b631be4
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-29 06:15:18 +0600

    project setup and fake data generation

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import "./App.css";
+import Banner from "./Component/Banner";
+import Footer from "./Component/Footer";
+import NavBar from "./Component/Navbar";
+
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar/>
+    <Banner/>
+    <Footer/>
+      
+    </>
+  );
+}
+
+export default App;
